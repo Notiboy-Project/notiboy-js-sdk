84e0b46e072d7b7378aaed4098e437fda88c7aab
Vidhyanand CS <vithushaji@gmail.com>
2022-11-10 13:14:13 +0530

    opt-in and opt-out using boxes

--- src/sdk.ts
+++ src/sdk.ts
@@ -15,7 +15,7 @@ export default class SDK extends RPC {
   }
 
   //Creating a logic sig for channel
-  async createLogicSig(channelName: string): Promise<algosdk.LogicSigAccount>{
+  async createLogicSig(channelName: string): Promise<algosdk.LogicSigAccount> {
     const teal = LsigTeal(channelName);
     const results = await this.client.compile(teal).do();
     const program = new Uint8Array(Buffer.from(results.result, "base64"));
@@ -41,51 +41,169 @@ export default class SDK extends RPC {
 
   // Optin to SC for channel creation
   async optin(
-    // name can be either dApp name or user 
-    //optin address is the logic sig address 
+    // name can be either dApp name or user
+    //optin address is the logic sig address
     //address is the creator address
-    channelName:string,
-    optinAddress:string,
-    address:string,
-    appArg:string
+    channelName: string,
+    optinAddress: string,
+    address: string,
+    appArg: string
   ): Promise<algosdk.Transaction[]> {
     //TODO: dapp name validations if necessary
     if (!this.isValidAddress(address)) {
       throw new Error("Invalid address");
     }
     let appArgs = [];
+    let boxes: any;
     if (channelName == "") {
       appArgs = [this.convertToIntArray(appArg)];
+      boxes = [
+        // use public key as box name
+        [0, algosdk.decodeAddress(address)],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+      ];
     } else {
       appArgs = [
         this.convertToIntArray(appArg),
         this.convertToIntArray(channelName),
       ];
+      boxes = [
+        [0, channelName],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+      ];
     }
     const params = await this.client.getTransactionParams().do();
-    params.fee = 2000;
+    params.fee = 5000;
     params.flatFee = true;
-    const paymentTxn = algosdk.makePaymentTxnWithSuggestedParams(
-      address,
-      DAPP_ESCROW,
-      OPTIN_FEE,
-      undefined,
-      undefined,
-      params,
-      undefined
-    );
-
-    const optinTxn = algosdk.makeApplicationOptInTxn(
-      optinAddress,
-      params,
-      APP_INDEX,
-      appArgs
-    );
-    optinTxn.fee = 0;
-    const groupTxns = [paymentTxn, optinTxn];
-
+    const txn2 = algosdk.makeApplicationNoOpTxnFromObject({
+      from: optinAddress,
+      suggestedParams: params,
+      appIndex: APP_INDEX,
+      appArgs: appArgs,
+      boxes: boxes,
+    });
+    const txn3 = algosdk.makeApplicationNoOpTxnFromObject({
+      from: optinAddress,
+      suggestedParams: params,
+      appIndex: APP_INDEX,
+      note: this.encodeString("Notiboy optout txn3"),
+      boxes: boxes,
+    });
+    txn3.fee = 0;
+    const txn4 = algosdk.makeApplicationNoOpTxnFromObject({
+      from: optinAddress,
+      suggestedParams: params,
+      appIndex: APP_INDEX,
+      note: this.encodeString("Notiboy optout txn4"),
+      boxes: boxes,
+    });
+    txn4.fee = 0;
+    const txn5 = algosdk.makeApplicationNoOpTxnFromObject({
+      from: optinAddress,
+      suggestedParams: params,
+      appIndex: APP_INDEX,
+      note: this.encodeString("Notiboy optout txn5"),
+      boxes: boxes,
+    });
+    txn5.fee = 0;
+    const txn1 = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+      amount: 1000000,
+      from: address,
+      suggestedParams: params,
+      to: DAPP_ESCROW,
+    });
+    const groupTxns = [txn1, txn2, txn3, txn4, txn5];
     algosdk.assignGroupID(groupTxns);
+    return groupTxns;
+  }
+  //Opt-out from channels
+  async optOut(
+    address: string,
+    channelName: string,
+    appArg: string
+  ): Promise<algosdk.Transaction[]> {
+    if (!this.isValidAddress(address)) {
+      throw new Error("Invalid address");
+    }
+    let appArgs = [];
+    let boxes: any;
+    if (channelName == "") {
+      appArgs = [this.convertToIntArray(appArg)];
+      boxes = [
+        // use public key as box name
+        [0, algosdk.decodeAddress(address)],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+      ];
+    } else {
+      appArgs = [
+        this.convertToIntArray(appArg),
+        this.convertToIntArray(channelName),
+      ];
+      boxes = [
+        [0, channelName],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+        [0, ""],
+      ];
+    }
+    const params = await this.client.getTransactionParams().do();
+    params.fee = 1000;
+    params.flatFee = true;
+    let note: any;
+    //create unsigned transaction
+    const txn2 = algosdk.makeApplicationCloseOutTxnFromObject({
+      from: address,
+      suggestedParams: params,
+      appIndex: APP_INDEX,
+      appArgs: appArgs,
+      boxes: boxes,
+    });
+    const txn3 = algosdk.makeApplicationNoOpTxnFromObject({
+      from: address,
+      suggestedParams: params,
+      appIndex: APP_INDEX,
+      note: this.encodeString("Notiboy optout txn3"),
+      boxes: boxes,
+    });
+    const txn4 = algosdk.makeApplicationNoOpTxnFromObject({
+      from: address,
+      suggestedParams: params,
+      appIndex: APP_INDEX,
+      note: this.encodeString("Notiboy optout txn4"),
+      boxes: boxes,
+    });
+    const txn5 = algosdk.makeApplicationNoOpTxnFromObject({
+      from: address,
+      suggestedParams: params,
+      appIndex: APP_INDEX,
+      note: this.encodeString("Notiboy optout txn5"),
+      boxes: boxes,
+    });
 
+    const groupTxns = [txn2, txn3, txn4, txn5];
+    algosdk.assignGroupID(groupTxns);
     return groupTxns;
   }
   // Get list of public channels
